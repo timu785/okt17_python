d5ceadcdef962e69caae2b38476d04c05e3558a1
timu785 <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2024-10-17 08:49:29 +0200

    1. és 2. listás feladat

---
+++ feladatok.py
@@ -0,0 +1,30 @@
+'''
+adott egy lista
+lista=[12, 21, 56, 32, -5, -23, 35]
+Az alábbi metódusok paraméterként kapják a fenti listát.
+1. Hány pozitív szám van benne?
+2. Mennyi a negatív számok összege?
+3. Mennyi az öttel osztható számok átlaga?
+'''
+
+def egy(lista):
+    db:int=0
+    i:int=1
+    while(i<=len(lista)):
+        if(lista[i-1]>0): db+=1
+        i+=1
+    return db
+
+def ketto(lista):
+    ossz:int=0
+    i:int=1
+    while(i<=len(lista)):
+        if(lista[i-1]<0): ossz+=lista[i-1]
+        i+=1
+    return ossz
+
+def ketto(lista):
+    ossz:int=0
+
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import feladatok
+
+lista=[12, 21, 56, 32, -5, -23, 35]
+
+print("1. feladat")
+print(f"A pozitív számok száma: {feladatok.egy(lista)}")
+
+print("2. feladat")
+print(f"A negatív számok összege: {feladatok.ketto(lista)}")
+
+print("3. feladat")
+
